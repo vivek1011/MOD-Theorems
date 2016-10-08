63e5e78de4dfb647319c2e00f0696965483cc2db
Vivek Bansal <bansalvivek79@gmail.com>
2016-10-09 02:03:25 +0530

    Create MOD.cpp

---
+++ MOD.cpp
@@ -0,0 +1,12 @@
+Fermat's theorem : 
+
+If a is any integer , then for a prime p : (a ^ p) % p = a 
+
+If a and p are coprime to each other , then comes Euler's theorem ,
+
+ (a ^ etf(p)) % p = 1 
+ 
+Now if there is a exponent a ^ x where x is going outerbound , 
+then you need to calculate (a ^ (x % etf(p))) % p because a^etf(p) % p = 1 
+
+SPOJ - POWPOW
